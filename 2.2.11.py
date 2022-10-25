bb505f74602766dc8cf28c8363d8ff38763bfdf7
Melnilov_Anton <ulukitkan1871@gmail.com>
2022-10-25 17:17:08 +0300

    Added task

---
+++ 2.2.11.py
@@ -0,0 +1,28 @@
+'''
+Напишите функцию принимающую на вход массив чисел и два числа `treshold` и `width`,
+а возвращающую списком индексы пиков которые превышают `treshold` и вокруг которых
+на растоянии `width/2` нет больших пиков.
+'''
+
+def find_peak(list1: list, treshold: float, width: float) -> list:
+    selection1 = []
+    for i in range(len(list1)):
+        if list1[i] > treshold:
+            selection1.append(i)
+    selection2 = []
+    if len(selection1) >= 2:
+        if selection1[0] + width / 2 < selection1[1]:
+            selection2.append(selection1[0])
+        for i in range(1, len(selection1) - 1):
+            if (selection1[i - 1] < selection1[i] - width/2) and (selection1[i + 1] > selection1[i] + width/2):
+                selection2.append(selection1[i])
+        if selection1[-1] - width / 2 > selection1[-2]:
+            selection2.append(selection1[-1])
+    else:
+        selection2 = selection1.copy()
+    return selection2
+
+#a = list(map(int, input().split()))
+#t = int(input())
+#w = int(input())
+#print(find_peak(a, t, w))
